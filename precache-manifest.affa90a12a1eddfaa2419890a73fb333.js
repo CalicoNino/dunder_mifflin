897abc7b3ba24b96d3b98c7aa5367de890017191
elninokr <karimelnino69@gmail.com>
2020-11-11 02:34:41 -0500

    Updates

---
+++ precache-manifest.affa90a12a1eddfaa2419890a73fb333.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30c6f9876ecaf510f2ef2be9da7180de",
+    "url": "/dunder_mifflin/index.html"
+  },
+  {
+    "revision": "81c26f51af74bb54af11",
+    "url": "/dunder_mifflin/static/css/2.6414bb7b.chunk.css"
+  },
+  {
+    "revision": "e9f7d0838c5c99a28419",
+    "url": "/dunder_mifflin/static/css/main.3322ffa9.chunk.css"
+  },
+  {
+    "revision": "81c26f51af74bb54af11",
+    "url": "/dunder_mifflin/static/js/2.8a5529d6.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/dunder_mifflin/static/js/2.8a5529d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e9f7d0838c5c99a28419",
+    "url": "/dunder_mifflin/static/js/main.be7339f6.chunk.js"
+  },
+  {
+    "revision": "7fa7e5124504d1d5bf1e",
+    "url": "/dunder_mifflin/static/js/runtime-main.10313cdc.js"
+  },
+  {
+    "revision": "2530d53c82d079344b78f3de5622ecf6",
+    "url": "/dunder_mifflin/static/media/dundee.2530d53c.jpg"
+  },
+  {
+    "revision": "1174c2c4a0d22893e626c0295e39c24e",
+    "url": "/dunder_mifflin/static/media/logo.1174c2c4.jpg"
+  },
+  {
+    "revision": "514ea42c3b02c26e2e7ad352bea8ed15",
+    "url": "/dunder_mifflin/static/media/mcpaper.514ea42c.png"
+  },
+  {
+    "revision": "48d816dc57ef540c33884612ab37fd26",
+    "url": "/dunder_mifflin/static/media/movie.48d816dc.jpg"
+  },
+  {
+    "revision": "91f030b15671e8897397cec0f5fb4ead",
+    "url": "/dunder_mifflin/static/media/paper.91f030b1.jpg"
+  },
+  {
+    "revision": "d6a0938819986de45d9f6ec305df3767",
+    "url": "/dunder_mifflin/static/media/volleyball.d6a09388.jpg"
+  }
+]);
